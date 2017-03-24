f64a99eb184eecd1ac8df8c5c72645e69ad18377
jin yan jie <1052048733.qq.com>
2017-03-24 13:38:50 +0800

    sucess ---

--- app/src/main/java/com/jin/myapplication/MainActivity.java
+++ app/src/main/java/com/jin/myapplication/MainActivity.java
@@ -1,15 +1,21 @@
 package com.jin.myapplication;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements MyAdapter.SolveData {
+public class MainActivity extends AppCompatActivity implements MyAdapter.SolveData{
+    private static final String TAG = "MainActivity";
         private RecyclerView recyclerView;
+    MyAdapter adapter;
     private List<GreensTypes> lists=new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,8 +25,9 @@ public class MainActivity extends AppCompatActivity implements MyAdapter.SolveDa
         recyclerView= (RecyclerView) findViewById(R.id.recycle);
         LinearLayoutManager manager=new LinearLayoutManager(this);
         recyclerView.setLayoutManager(manager);
-        MyAdapter adapter=new MyAdapter(lists,this);
+        adapter=new MyAdapter(lists,this);
         recyclerView.setAdapter(adapter);
+
     }
 
     /**
@@ -28,12 +35,37 @@ public class MainActivity extends AppCompatActivity implements MyAdapter.SolveDa
      */
     public void getData(){
         lists.add(new GreensTypes("yi"));
+        lists.add(new GreensTypes("er"));
+        lists.add(new GreensTypes("san"));
     }
 
+//    这里用一个views保存各个items
+    private List<View> views=new ArrayList<>();
     @Override
-    public void solve(MyAdapter.ViewHolder holder, int position, List<GreensTypes> lists) {
+    public void solve(final MyAdapter.ViewHolder holder, final int position, List<GreensTypes> lists) {
+        final GreensTypes greensTypes=lists.get(position);
+//        这里是把item布局添加到views里面
+        views.add(holder.linearLayout);
+        Log.e(TAG, "solve: views.size()=="+views.size() );
+        final int size=lists.size();
+
+
 //        lists里面存的是GreensTypes，菜系。
-        GreensTypes greensTypes=lists.get(position);
-        holder.greenType.setText(greensTypes.getName());
+        holder.linearLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                点击的时候判断点击的是哪个，如果是就把背景设置为蓝色，否则就让其他item背景为白色。
+                for (int i=0;i<size;i++){
+                    if (i==position){
+                        Log.e(TAG, "onClick: "+"变成蓝色" );
+                        holder.linearLayout.setBackgroundColor(Color.BLUE);
+                        holder.greenType.setText(greensTypes.getName());
+                    }else {
+                        views.get(i).setBackgroundColor(Color.WHITE);
+                    }
+                    adapter.notifyDataSetChanged();
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/jin/myapplication/MyAdapter.java
+++ app/src/main/java/com/jin/myapplication/MyAdapter.java
@@ -3,28 +3,35 @@ package com.jin.myapplication;
 import android.content.Context;
 import android.media.Image;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.List;
+
+import static android.content.ContentValues.TAG;
 
 /**
  * Created by jyj on 2017/3/23.
  */
 
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder>{
+    private static final String TAG = "MyAdapter";
     private List<GreensTypes> lists;
 //    这里是通过构造函数，让MainActivity实现
     public MyAdapter(List<GreensTypes> lists,  SolveData solveData) {
+        Log.e(TAG, "MyAdapter: " );
         this.lists = lists;
         this.solveData = solveData;
     }
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Log.e(TAG, "onCreateViewHolder: " );
         View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.type_item,parent,false);
         ViewHolder viewHolder=new ViewHolder(view);
         return viewHolder;
@@ -32,21 +39,26 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder>{
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
+        Log.e(TAG, "onBindViewHolder: " );
         solveData.solve(holder, position,lists);
     }
 
     @Override
     public int getItemCount() {
+        Log.e(TAG, "getItemCount: " );
         return lists.size();
     }
 
     static  class ViewHolder extends RecyclerView.ViewHolder{
         ImageView imageView;
         TextView greenType;
+        LinearLayout linearLayout;
         public ViewHolder(View itemView) {
             super(itemView);
+            Log.e(TAG, "ViewHolder: " );
             imageView= (ImageView) itemView.findViewById(R.id.img);
             greenType= (TextView) itemView.findViewById(R.id.green_type);
+            linearLayout= (LinearLayout) itemView.findViewById(R.id.line1);
         }
     }
 //    这是写的一个接口，用于把这些方法传到MainActivity里面。
